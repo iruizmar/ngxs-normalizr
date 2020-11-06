a34e34415f731aeb5f305eaa407c4918707a7f21
Ignacio Ruiz <iruizm@hiberus.com>
2020-11-06 17:36:27 +0100

    Final version of the article's code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsModule } from '@ngxs/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonTypeModule } from './pokemon-type/pokemon-type.module';
+import { PokemonTypesState } from './pokemon-type/pokemon-types.state';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { PokemonsState } from './pokemon/pokemons.state';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PokemonModule,
+    PokemonTypeModule,
+    NgxsModule.forRoot([PokemonTypesState, PokemonsState]),
+    NgxsReduxDevtoolsPluginModule.forRoot(),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
